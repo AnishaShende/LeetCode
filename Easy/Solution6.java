1d249de90c650d4531450f3849da7405f6ca69e4
Anisha Shende <91362696+AnishaShende@users.noreply.github.com>
2022-11-19 23:53:06 +0530

    6

---
+++ Easy/Solution6.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class Solution6 {
+    public String addBinary(String a, String b) {
+        int n1 = Integer.parseInt(a);
+        int n2 = Integer.parseInt(b);
+        // int add = n1 + n2;
+        // String ans = Integer.toBinaryString(add);
+        // return ans;
+        String num1 = Integer.toBinaryString(n1);
+        String num2 = Integer.toBinaryString(n2);
+        
+        // //int res = Integer.toBinaryString(num1+num2);
+        // int res = Integer.parseInt(num1)+Integer.parseInt(num2);
+        // String a = res.toString();
+        // return res;
+    }
+}
