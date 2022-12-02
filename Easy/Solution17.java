090a0fd0339a8c7fe0e476078f5bb3ce626c26ec
Anisha Shende <91362696+AnishaShende@users.noreply.github.com>
2022-12-02 20:45:38 +0530

    17

---
+++ Easy/Solution17.java
@@ -0,0 +1,19 @@
+class Solution17 {
+    public String restoreString(String s, int[] indices) {
+        String str = new String();
+        for(int i = 0; i < indices.length; i ++){
+            for(int j = 0; j < indices.length; j ++){
+                if(i == indices[j]){
+                    str += s.charAt(j);
+                }
+            }
+        }
+        return str;
+    }
+    public static void main(String[] args) {
+        Solution17 obj = new Solution17();
+        int arr[] = {4,5,6,7,0,2,1,3};
+        String s = obj.restoreString("codeleet", arr);
+        System.out.println(s);
+    }
+}
