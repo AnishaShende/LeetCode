364426a98f6dbe7fb3fb53aceb206246e594a9bf
Anisha Shende <anishashende963@gmail.com>
2023-11-10 23:59:11 +0530

    Leetcode day 9!

---
+++ Easy/StringPixels.java
@@ -0,0 +1,35 @@
+// 806. Number of Lines To Write String
+
+public class StringPixels {
+    public int[] numberOfLines(int[] widths, String s) {
+        int[] outarr = new int[2];
+        int lines = 1;
+        int pixels = 0;
+        int count = 0;
+        System.out.println("-");
+        for (int i = 0; i < widths.length; i++) {
+            if (count < 100) {
+                count += widths[i];
+                pixels = count;
+                // System.out.println("i = " + i + ", pixels = " + pixels);
+            } else {
+                lines++;
+                pixels = count;
+                // pixels += widths[i];
+                count = 0;
+            }
+        }
+        outarr[0] = lines;
+        outarr[1] = pixels;
+        return outarr;
+    }
+
+    public static void main(String[] args) {
+        StringPixels obj = new StringPixels();
+        int[] widths = { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+                10, 10 };
+        String s = "abcdefghijklmnopqrstuvwxyz";
+        int[] result = obj.numberOfLines(widths, s);
+        System.out.println("[" + result[0] + ", " + result[1] + "]");
+    }
+}
